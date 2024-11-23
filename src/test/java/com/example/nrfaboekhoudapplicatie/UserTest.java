c19ff8f5d6fa3889a2912d73de42f9a325b011a8
Fantasyfays <134273551+Fantasyfays@users.noreply.github.com>
2024-11-23 17:48:11 +0100

    Add specific tests for roles in User entity

--- src/test/java/com/example/nrfaboekhoudapplicatie/UserTest.java
+++ src/test/java/com/example/nrfaboekhoudapplicatie/UserTest.java
@@ -5,10 +5,18 @@ import com.example.nrfaboekhoudapplicatie.dal.entity.User;
 import com.example.nrfaboekhoudapplicatie.enums.RoleType;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
  class UserTest {
+
+     @Test
+     void testRolesInitialized() {
+         User user = new User();
+         assertEquals(new HashSet<>(), user.getRoles(),"roles moet leeg geinitialeesrd zijn");
+     }
 
     @Test
      void testAddRole() {
