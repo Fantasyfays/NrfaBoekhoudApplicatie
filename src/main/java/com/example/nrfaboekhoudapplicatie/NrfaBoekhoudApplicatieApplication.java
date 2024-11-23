4dda4c63fa64278a500bb905f07057f4c459f7c6
Fantasyfays <134273551+Fantasyfays@users.noreply.github.com>
2024-11-23 14:54:13 +0100

    Initial commit

---
+++ src/main/java/com/example/nrfaboekhoudapplicatie/NrfaBoekhoudApplicatieApplication.java
@@ -0,0 +1,13 @@
+package com.example.nrfaboekhoudapplicatie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NrfaBoekhoudApplicatieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NrfaBoekhoudApplicatieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nrfaboekhoudapplicatie/NrfaBoekhoudApplicatieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nrfaboekhoudapplicatie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NrfaBoekhoudApplicatieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
